b5d5a6af15e1b172c6ad369a8fcf27679355bae7
alee chou <zq@alee2002.com>
2013-03-03 14:54:04 +0800

    it can be run

---
+++ example/parse-html.js
@@ -0,0 +1,22 @@
+require('seajs') ;
+
+define(function(require){
+
+	var Parser = require("../lib/Parser.js") ;
+
+	require("fs").readFile(__dirname+"/templates/css-zen-garden.html",function(err,bff){
+
+		if(err){
+			throw new Error(err) ;
+		}
+
+		var parser = new Parser ;
+		parser.parseSync(bff) ;
+
+	})
+
+
+
+}) ;
+
+
