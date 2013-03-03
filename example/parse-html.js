457916dac3fdcffd892dd8a841d05a38c47f79fc
alee chou <zq@alee2002.com>
2013-03-04 00:24:27 +0800

    add Factory class

--- example/parse-html.js
+++ example/parse-html.js
@@ -2,7 +2,7 @@ require('seajs') ;
 
 define(function(require){
 
-	var Parser = require("../") ;
+	var ParserFactory = require("../") ;
 
 	require("fs").readFile(__dirname+"/templates/css-zen-garden.html",function(err,bff){
 
@@ -10,7 +10,7 @@ define(function(require){
 			throw new Error(err) ;
 		}
 
-		var parser = Parser.htmlParser() ;
+		var parser = ParserFactory.htmlParser() ;
 		var objTree = parser.parseSync(bff) ;
 
 		console.log(objTree) ;
