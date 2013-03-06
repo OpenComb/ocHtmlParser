3a500291e4b16087595521d2841f56e4b1076bd7
alee chou <zq@alee2002.com>
2013-03-06 19:57:12 +0800

    refact the parse mechanism

--- example/parse-html.js
+++ example/parse-html.js
@@ -4,14 +4,19 @@ define(function(require){
 
 	var ParserFactory = require("../") ;
 
-	require("fs").readFile(__dirname+"/templates/test.html",function(err,bff){
+	require("fs").readFile(__dirname+"/templates/css-zen-garden.html",function(err,bff){
 
 		if(err){
 			throw new Error(err) ;
 		}
 
 		var parser = ParserFactory.htmlParser() ;
-		var objTree = parser.parseSync(bff) ;
+		try{
+			var objTree = parser.parseSync(bff) ;
+		}catch(e){
+			console.log(e.stack) ;
+			console.log(e.toString()) ;
+		}
 
 		console.log(objTree) ;
 
