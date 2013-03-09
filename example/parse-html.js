9365477c2e85ef746659c2efa0a87e397fde4e43
alee chou <zq@alee2002.com>
2013-03-09 22:54:45 +0800

    drop use seajs

--- example/parse-html.js
+++ example/parse-html.js
@@ -1,29 +1,24 @@
-require('seajs') ;
-
-define(function(require){
-
-	var ParserFactory = require("../") ;
-
-	require("fs").readFile(__dirname+"/templates/css-zen-garden.html",function(err,bff){
-
-		if(err){
-			throw new Error(err) ;
-		}
-
-		var parser = ParserFactory.htmlParser() ;
-		try{
-			var objTree = parser.parseSync(bff) ;
-		}catch(e){
-			console.log(e.stack) ;
-			console.log(e.toString()) ;
-		}
-
-		console.log(objTree) ;
-
-	})
 
 
+var ParserFactory = require("../") ;
 
-}) ;
+require("fs").readFile(__dirname+"/templates/css-zen-garden.html",function(err,bff){
+
+	if(err){
+		throw new Error(err) ;
+	}
+
+	var parser = ParserFactory.htmlParser() ;
+	try{
+		var objTree = parser.parseSync(bff) ;
+	}catch(e){
+		console.log(e.stack) ;
+		console.log(e.toString()) ;
+	}
+
+	console.log(objTree) ;
+
+})
+
 
 
